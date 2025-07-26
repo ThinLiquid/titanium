524642e0ff8208ef300f54cc86ebcc93c7211e44
ThinLiquid <thnlqd@gmail.com>
2025-07-26 08:33:11 +0200

    fixed iframe css

--- src/components/body/main.tsx
+++ src/components/body/main.tsx
@@ -4,13 +4,8 @@ import type { Component } from 'solid-js';
 const Body: Component = () => {
   return (
 
-    <div id="mainBody" style={{
-      display: "flex",
-      flex: 1
-    }}>
-      <iframe id="mainBodyFrame" height="100%" width="100%" style={{
-      border: "none"
-    }}></iframe>
+    <div id='mainBody' style={{ display: 'flex', height: '100%' }}>
+      <iframe height='100%' width='100%' style={{ border: 'none' }}></iframe>
     </div>
   );
 };
